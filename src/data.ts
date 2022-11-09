ae0cfffd8e26d9d20cb9e33a34f5e6ee4388d39f
Rafael Castilho de Castro <raphaeldavant@hotmail.com>
2022-11-09 14:50:16 -0300

    Feat: Add get all users & post new user Endpoints

---
+++ src/data.ts
@@ -0,0 +1,70 @@
+export type transaction = {
+    date: string,
+    amount: number,
+    description: string
+}
+
+export type userAccount = {
+    name: string,
+    cpf: number,
+    birthdate: string,
+    balance: number,
+    transactions: transaction[]
+}
+
+export let users: userAccount[] = [
+    {
+        name: 'Maria',
+        cpf: 12345678901,
+        birthdate: '01/01/1950',
+        balance: 1000,
+        transactions: [
+            {
+                date: '02/02/2002',
+                amount: 10,
+                description: 'Mercado'
+            }
+        ]
+    },
+    {
+        name: 'Martha',
+        cpf: 23456789123,
+        birthdate: '10/02/1960',
+        balance: 100,
+        transactions: [
+            {
+                date: '01/05/2000',
+                amount: 50,
+                description: 'Pet-shop'
+            },
+            {
+                date: '12/07/2004',
+                amount: 15,
+                description: 'Hot-dog'
+            }
+        ]
+    },
+    {
+        name: 'Eva',
+        cpf: 34567891234,
+        birthdate: '25/07/1975',
+        balance: 550,
+        transactions: [
+            {
+                date: '03/09/2021',
+                amount: 90,
+                description: 'Pizza'
+            },
+            {
+                date: '04/08/2001',
+                amount: 101,
+                description: 'Calça jeans'
+            },
+            {
+                date: '02/09/2010',
+                amount: 500,
+                description: 'Vinhos'
+            }
+        ]
+    }
+]
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,41 @@
 import express, { Request, Response } from "express"
 import cors from 'cors'
+import { userAccount, users } from "./data"
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 
+
+app.get("/users", (req: Request, res: Response) => {
+    res.status(200).send(users)
+})
+
+app.post("/users", (req: Request, res: Response) => {
+    const name = req.body.name
+    const cpf = req.body.cpf
+    const birthdate = req.body.birthdate
+
+    const userBirthInMilisec = new Date(birthdate).getTime()
+    const eighteenYearsInMilisec = 568080000000
+    const todayInMilisec = Date.now()
+
+    if ((todayInMilisec-userBirthInMilisec)<eighteenYearsInMilisec) {
+        res.status(400).send("User must be over 18.")
+    } else {
+        const newUser: userAccount = {
+            name,
+            cpf,
+            birthdate,
+            balance: 0,
+            transactions: []
+        }
+
+        users.push(newUser)
+
+        res.status(200).send(users)
+    }
+})
 
 
 app.listen(3003, () => {
