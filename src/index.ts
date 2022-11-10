c7928cd8b9ebf8befd2a2b444c2eae65b26327dc
Rafael Castilho de Castro <raphaeldavant@hotmail.com>
2022-11-10 15:07:55 -0300

    Feat: Adds complementary exercises 1 to 3

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,25 @@ app.get("/users", (req: Request, res: Response) => {
     res.status(200).send(users)
 })
 
+app.get("/user/balance", (req: Request, res: Response) => {
+    const cpf = Number(req.query.cpf)
+
+    let userBalance = 0
+    let userExists = false
+    for (const i of users) {
+        if (i.cpf === cpf) {
+            userBalance = i.balance
+            userExists = true
+        }
+    }
+    if (userExists) {
+        res.status(200).send(`User balance:${userBalance}`)
+    } else {
+        res.status(400).send('CPF not found.')
+    }
+
+})
+
 app.post("/users", (req: Request, res: Response) => {
     const name = req.body.name
     const cpf = req.body.cpf
@@ -20,20 +39,47 @@ app.post("/users", (req: Request, res: Response) => {
     const eighteenYearsInMilisec = 568080000000
     const todayInMilisec = Date.now()
 
-    if ((todayInMilisec-userBirthInMilisec)<eighteenYearsInMilisec) {
+    if ((todayInMilisec - userBirthInMilisec) < eighteenYearsInMilisec) {
         res.status(400).send("User must be over 18.")
     } else {
-        const newUser: userAccount = {
-            name,
-            cpf,
-            birthdate,
-            balance: 0,
-            transactions: []
+        let userExists = false
+        for (const i of users) {
+            if (i.cpf === cpf) {
+                userExists = true
+            }
         }
+        if (!userExists) {
+            const newUser: userAccount = {
+                name,
+                cpf,
+                birthdate,
+                balance: 0,
+                transactions: []
+            }
+            users.push(newUser)
+            res.status(200).send(users)
+        } else {
+            res.status(400).send('CPF already registered.')
+        }
+    }
+})
 
-        users.push(newUser)
+app.put("/user", (req:Request, res:Response)=>{
+    const userName = req.body.name
+    const cpf = req.body.cpf
+    const amount = req.body.amount
 
-        res.status(200).send(users)
+    let userFound = false
+    for (const i of users) {
+        if (i.name === userName && i.cpf === cpf) {
+            i.balance = i.balance+amount
+            userFound = true
+        }
+    }
+    if (userFound) {
+        res.status(200).send(`$${amount} deposited into ${userName}'s account.`)
+    } else {
+        res.status(400).send("No user found.")
     }
 })
 
